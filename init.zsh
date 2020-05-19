9a5a7625a4391637008fd5d2626b4836c65de357
Jeeva <jeevatatva@users.noreply.github.com>
2020-05-20 01:45:11 +0530

    macOS only.

--- init.zsh
+++ init.zsh
@@ -85,20 +85,6 @@ alias df='df -h'
 alias du='du -h'
 
 
-#
-# GNU only
-#
-
-if (( ${+commands[dircolors]} )); then
-
-  alias lx='ll -X' # long format, sort by extension
-
-  # Always wear a condom
-  alias chmod='chmod --preserve-root -v'
-  alias chown='chown --preserve-root -v'
-fi
-
-
 # not aliasing rm -i, but if safe-rm is available, use condom.
 # if safe-rmdir is available, the OS is suse which has its own terrible 'safe-rm' which is not what we want
 if (( ${+commands[safe-rm]} && ! ${+commands[safe-rmdir]} )); then
